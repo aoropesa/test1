bda7268d98798ad9e204ed7ca68a7a92a8510bf1
Alexander Oropesa <Alexander Oropesa>
2017-05-25 21:24:59 -0400

    first commit

---
+++ Q3.js
@@ -0,0 +1,10 @@
+// Question 3
+
+// Create a new variable called bar and assign to it the function definition of foo.
+
+// Then call console.log and pass it the return value of passing 3 to bar.
+var bar = foo
+function foo(num) {
+  return num + 2;
+}
+console.log(bar(3));
